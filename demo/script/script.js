57e7d2ea82ca24260d33fdf7c588a1f9ab361690
Hariprasath1996 <hasathphy96@gmail.com>
2023-06-21 19:53:51 +0530

    add title

---
+++ demo/script/script.js
@@ -0,0 +1,73 @@
+`use strict`;
+// input
+const btn = document.getElementById(`btn`);
+console.log(btn);
+
+// const a = document.querySelector(`.btn3`);
+// console.log(a);
+// const b = document.querySelector(`.btn4`)
+// console.log(b);
+// const c = document.querySelector(`.btn5`)
+// console.log(c);
+
+// const d = document.querySelectorAll(`.btn`);
+// console.log(d)
+// const e = document.querySelectorAll(`.btn`);
+// console.log(e)
+// const divEl = document.getElementById("div");
+// const par = document.querySelector(`.model-paragraph`);
+// console.log(par);
+
+// divEl.innerText = 'staz';
+// par.innerHTML = "jana";
+
+// divEl.style.backgroundColor='red'
+// divEl.style.color='blue'
+
+// divEl.style.border='2px solid green'
+
+// const par = document.querySelector(`.model-paragraph`)
+// console.log(par);
+
+// par.innerHTML = `raju bhai`
+// div.innerHTML=`asdf`
+
+// div.style.backgroundcolor=`green`
+
+// const para = document.querySelector(`.model-paragraph`)
+// console.log(para);
+
+// const change = ()=>{
+//   para.innerHTML=`hari`
+//   para.style.backgroundColor = `yellow`
+//   para.style.color = `white`
+// }
+
+// bttn.addEventListener('click',change)
+
+// function sum (num1,num2){
+//   let add=num1+num2
+//   para.innerHTML=add
+//   console.log(add);
+// }
+
+// sum (2,3)
+// const bttn = document.querySelector(`.btn`);
+// console.log(bttn);
+
+// function msg(age) {
+//   if (age == 18) {
+//     console.log("you are a adult");
+//   } else if (age >= 19) {
+//     console.log("you can do this only");
+//   } else {
+//     console.log("you are a minor");
+//   }
+// }
+
+// const age = prompt("age?");
+// bttn.addEventListener(`click`, () => {
+//   msg(age);
+// });
+// msg(age)
+
