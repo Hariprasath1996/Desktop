7410c8883da27172d149ed9528f079f76aa67b8e
Hariprasath1996 <hasathphy96@gmail.com>
2023-07-26 21:25:42 +0530

    turbo

---
+++ coding challenge/script/script.js
@@ -0,0 +1,18 @@
+`use strict`
+
+// 1. Write a program that calculates the sum of two numbers and displays the result.
+function addNumbers(num1,num2) {
+  return num1+num2
+
+}
+addNumbers(2,3)
+let numOne = Number(prompt("add first number")) 
+let numTwo = Number(prompt("add second number")) 
+let sum = (numOne+numTwo);
+console.log(sum);
+
+
+
+
+
+
