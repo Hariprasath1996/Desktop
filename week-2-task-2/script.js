4e10c61571532a41473397f647fd3e7b54bc9cd1
Hariprasath1996 <hasathphy96@gmail.com>
2023-09-12 21:59:06 +0530

    one more task

--- week-2-task-2/script.js
+++ week-2-task-2/script.js
@@ -1,3 +1,25 @@
 `use strict`
 
+// Write a JavaScript function to check if a given number is even or odd.
 
+
+function givenNumberIsEvenOrOdd(num) {
+  if (num % 2 === 0 ) {
+ return `its even`
+  } else {
+  return `its odd`
+  }
+}
+
+console.log( givenNumberIsEvenOrOdd(1));
+console.log( givenNumberIsEvenOrOdd(15));
+console.log( givenNumberIsEvenOrOdd(18));
+console.log( givenNumberIsEvenOrOdd(4));
+
+// Write a JavaScript program to calculate the factorial of a number.
+
+
+
+
+
+// Write a JavaScript program to find the sum of all numbers in each array, excluding any numbers that are less than 0.
